c3a2d356439a0b8b652ff4a506f7e2b6b8bc1adf
OrlandoCodes10 <orlandofranco.novesteras@gmail.com>
2023-02-10 03:08:16 -0500

    Infinite loops examples - DO NOT RUN

--- app.js
+++ app.js
@@ -17,6 +17,12 @@
 //     console.log(i);
 // }
 
-for (let i = 100; i >=0; i -=10) {
-    console.log(i);
-}
+// for (let i = 100; i >=0; i -=10) {
+//     console.log(i);
+// }
+
+//Do not run thi code! 
+//for (let i = 20; i >=0; i++) {
+//     console.log(i);
+// }
+
