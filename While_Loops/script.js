4ef8d0a0f899c6cb460aea22c068067110218b6f
OrlandoCodes10 <orlandofranco.novesteras@gmail.com>
2023-02-11 23:29:06 -0500

    Completed the example

--- While_Loops/script.js
+++ While_Loops/script.js
@@ -1,2 +1,22 @@
 let maximum = parseInt(prompt("Enter the maximum number!"));
+while (!maximum) {
+    maximum = parseInt(prompt("Enter a valid number!"));
+}
 
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+
+let guess = parseInt(prompt("enter your first guess!"));
+let attemps = 1;
+
+while (parseInt(guess) !== targetNum) {
+    if (guess === 'q') break;
+    attemps++;
+    if (guess > targetNum) {
+        guess = (prompt("Too high! Enter a new guess:"))
+    } else {
+        guess = (prompt("Too low! Enter a new guess:"))
+ 
+    }
+if (guess === 'q')
+}
+console.log(`You got it! It took you ${attemps} guesses`);
