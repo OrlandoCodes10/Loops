480d1185fb90a06c25ec7ea5bef195bb3b213576
OrlandoCodes10 <orlandofranco.novesteras@gmail.com>
2023-02-13 01:41:30 -0500

    More practice with for of iterating arrays

--- ForOf/app.js
+++ ForOf/app.js
@@ -3,25 +3,40 @@
 
 //}
 
-const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer']
+// const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer']
 
-for (let i = 0; i < subreddits.length; i++) {
-    console.log(`Visit reddit.com/r/${subreddits[i]}`)
-}
+// for (let i = 0; i < subreddits.length; i++) {
+//     console.log(`Visit reddit.com/r/${subreddits[i]}`)
+// }
 
-for (let subreddit of subreddits) {
-    console.log(`Visit reddit.com/r/${subreddit}`)
-}
+// for (let subreddit of subreddits) {
+//     console.log(`Visit reddit.com/r/${subreddit}`)
+// }
 
-const seatingChart = [
-    ['Kristen', 'Erik', 'Namita'],
-    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
-    ['Yuma', 'Sakura', 'Jack', 'Erika']
-]
+// const seatingChart = [
+//     ['Kristen', 'Erik', 'Namita'],
+//     ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+//     ['Yuma', 'Sakura', 'Jack', 'Erika']
+// ]
 
-for (let i = 0; i < seatingChart.length; i++) {
-    const row = seatingChart[i];
-    for (let j = 0; j < row.lenght; j++) {
-        console.low(row[j])
-    }
-}
+// for (let i = 0; i < seatingChart.length; i++) {
+//     const row = seatingChart[i];
+//     for (let j = 0; j < row.lenght; j++) {
+//         console.low(row[j])
+//     }
+// }
+
+// //rewrite
+
+// for (let row of seatingChart) {
+//     for (let student of row) {
+//         console.log(student);
+//     }
+// }
+
+// for (let char of "hellow world") {
+//     console.log(char)
+// }
+
+// ex. of iterating objects
+
