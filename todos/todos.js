ea2d5685e58a3c7a93f0202705e27902686ab8d8
OrlandoCodes10 <orlandofranco.novesteras@gmail.com>
2023-02-14 04:24:50 -0500

    Completed the todo app walk through

--- todos/todos.js
+++ todos/todos.js
@@ -9,7 +9,17 @@ while (input !== 'quit' && input !== 'q') {
         console.log('*********')
     } else if (input ==='new') {
             const newTodo = prompt('OK, what is the new todo?');
-            console.log(`${newTodo}`)
-    }input = prompt('what would you like to do?')
+            todos.push(newTodo);
+            console.log(`${newTodo} added to the list`)
+    } else if (input === 'delete') {
+        const indexStr = prompt('Ok, enter an index to delete:');
+        if (!Number.isNaN(index)) {
+            const deleted = todos.splice(index, 1);
+            console.log(`Ok, deleted ${deleted[0]}`);
+        } else {
+            console.log('Unknown index')
+        }
+    }
+    input = prompt('what would you like to do?')
 }
 console.log('OK QUIT THE APP!')
